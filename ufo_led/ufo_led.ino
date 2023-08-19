02fd32b3525fff46076294a96c117ac7ce547f47
Zhongkai Ren <akyeren@gmail.com>
2023-08-19 21:59:43 +0300

    Refactor index and use auto

--- ufo_led/ufo_led.ino
+++ ufo_led/ufo_led.ino
@@ -1,4 +1,3 @@
-
 #include <FastLED.h>
 
 // ********************************************************
@@ -30,7 +29,7 @@
 #define NUM_LEDS_SMALL_RING 7
 #define SEGMENT_SIZE 3  // # of groups to divide the large ring into
 
-#define NUM_LEDS_LARGE_SEGMENT = NUM_LEDS_LARGE_RING / SEGMENT_SIZE
+#define NUM_LEDS_LARGE_SEGMENT NUM_LEDS_LARGE_RING / SEGMENT_SIZE
 
 // Pin configuration
 #define PIN_LED_LARGE_RING 4  // D4
@@ -68,154 +67,148 @@ byte gHueLargeRing = HUE_PURPLE;
 
 // **************************************************************************************************************
 void setup() {
-    delay(3000);  // power-up safety delay
+  delay(3000);  // power-up safety delay
 
-    FastLED.addLeds<LED_TYPE, PIN_LED_SMALL_RING, COLOR_ORDER>(
-        ledsSmallRing, NUM_LEDS_SMALL_RING);  //.setCorrection( TypicalLEDStrip );
-    FastLED.setBrightness(LED_BRIGHTNESS);
+  FastLED.addLeds<LED_TYPE, PIN_LED_SMALL_RING, COLOR_ORDER>(
+    ledsSmallRing, NUM_LEDS_SMALL_RING);  //.setCorrection( TypicalLEDStrip );
+  FastLED.setBrightness(LED_BRIGHTNESS);
 
-    FastLED.addLeds<LED_TYPE, PIN_LED_LARGE_RING, COLOR_ORDER>(
-        ledsLargeRing, NUM_LEDS_LARGE_RING);  //.setCorrection( TypicalLEDStrip );
-    FastLED.setBrightness(LED_BRIGHTNESS);
+  FastLED.addLeds<LED_TYPE, PIN_LED_LARGE_RING, COLOR_ORDER>(
+    ledsLargeRing, NUM_LEDS_LARGE_RING);  //.setCorrection( TypicalLEDStrip );
+  FastLED.setBrightness(LED_BRIGHTNESS);
 
-    Serial.begin(9600);
+  Serial.begin(9600);
+}
+
+void dimRingLed(int ring[], byte size, byte index, byte offset, byte dec) {
+  const auto target = (size + index - offset) % size;
+  ring[target] -= min(ring[target], dec);
 }
 
 // **************************************************************************************************************
-void setSmallRing(unsigned speed) {
-    static int iRing[] = {0, 0, 100, 0, 0, 0, 0};
-    static byte iPos = 2;
+void setSmallRing(byte speed) {
+  static int ring[] = { 0, 0, 100, 0, 0, 0, 0 };
+  static auto index = 2;  // 0 .. NUM_LEDS_SMALL_RING - 1
+  static const auto MaxVal = 200;
 
-    if (iRing[iPos % NUM_LEDS_SMALL_RING] >= 200) {
-        iRing[(iPos - 2) % NUM_LEDS_SMALL_RING] = 0;
-        ++iPos;
-    }
+  if (ring[index] >= MaxVal) {
+    ring[(NUM_LEDS_SMALL_RING + index - 2) % NUM_LEDS_SMALL_RING] = 0;
+    ++index;
+    index %= NUM_LEDS_SMALL_RING;
+  }
 
-    if (iRing[(iPos - 2) % NUM_LEDS_SMALL_RING] > 0) {
-        iRing[(iPos - 2) % NUM_LEDS_SMALL_RING] -= speed;
-    } else {
-        iRing[(iPos - 2) % NUM_LEDS_SMALL_RING] = 0;
-    }
+  dimRingLed(ring, NUM_LEDS_SMALL_RING, index, 1, speed);
+  dimRingLed(ring, NUM_LEDS_SMALL_RING, index, 2, speed);
 
-    if (iRing[(iPos - 1) % NUM_LEDS_SMALL_RING] > 0) {
-        iRing[(iPos - 1) % NUM_LEDS_SMALL_RING] -= speed;
-    } else {
-        iRing[(iPos - 1) % NUM_LEDS_SMALL_RING] = 0;
-    }
+  if (ring[index] < MaxVal)
+    ring[index] += speed;
 
-    iRing[(iPos + 0) % NUM_LEDS_SMALL_RING] += speed;
-
-    // Set ring
-    for (byte k = 0; k < NUM_LEDS_SMALL_RING; ++k) {
+  // Set ring
+  for (auto k = 0; k < NUM_LEDS_SMALL_RING; ++k) {
 #ifdef DBG_PRINT
-        Serial.print(" ");
-        Serial.print(iRing[k]);
+    Serial.print(" ");
+    Serial.print(ring[k]);
 #endif
-
-        if (iRing[k] < 0) {
-            iRing[k] = 0;
-        }
-
-        if (iRing[k] < 20) {
-            ledsSmallRing[k] = CRGB::Black;
-        } else {
-            if (gBrightness <= 1) {
-                ledsSmallRing[k] = CRGB::Black;
-            } else {
-                ledsSmallRing[k] = MakeHsv(gHueSmallRing, iRing[k]);
-                ledsSmallRing[k].fadeLightBy(255 - gBrightness);
-            }
-        }
+    if (ring[k] < 20) {
+      ledsSmallRing[k] = CRGB::Black;
+    } else {
+      if (gBrightness <= 1) {
+        ledsSmallRing[k] = CRGB::Black;
+      } else {
+        ledsSmallRing[k] = MakeHsv(gHueSmallRing, ring[k]);
+        ledsSmallRing[k].fadeLightBy(255 - gBrightness);
+      }
     }
+  }
 
 #ifdef DBG_PRINT
-    Serial.println(" ");
+  Serial.println(" ");
 #endif
 }
 
 // **************************************************************************************************************
 void showSolid() {
-    for (byte k = 0; k < NUM_LEDS_LARGE_RING; ++k) {
-        ledsLargeRing[k] = MakeHsv(gHueLargeRing, 255);
-        if (gBrightness <= 1) {
-            ledsLargeRing[k] = CRGB::Black;
-        } else {
-            ledsLargeRing[k].fadeLightBy(255 - gBrightness);
-        }
+  for (auto k = 0; k < NUM_LEDS_LARGE_RING; ++k) {
+    ledsLargeRing[k] = MakeHsv(gHueLargeRing, 255);
+    if (gBrightness <= 1) {
+      ledsLargeRing[k] = CRGB::Black;
+    } else {
+      ledsLargeRing[k].fadeLightBy(255 - gBrightness);
     }
+  }
 
-    for (byte k = 0; k < NUM_LEDS_SMALL_RING; ++k) {
-        ledsSmallRing[k] = MakeHsv(gHueSmallRing, 255);
-        if (gBrightness <= 1) {
-            ledsSmallRing[k] = CRGB::Black;
-        } else {
-            ledsSmallRing[k].fadeLightBy(255 - gBrightness);
-        }
+  for (auto k = 0; k < NUM_LEDS_SMALL_RING; ++k) {
+    ledsSmallRing[k] = MakeHsv(gHueSmallRing, 255);
+    if (gBrightness <= 1) {
+      ledsSmallRing[k] = CRGB::Black;
+    } else {
+      ledsSmallRing[k].fadeLightBy(255 - gBrightness);
     }
+  }
 
-    FastLED.show();
-    delay(100);
+  FastLED.show();
+  delay(100);
 }
 
 // **************************************************************************************************************
-void showRunningLights(word speed, byte largeRingIndex, byte level) {
-    // Large Ring
-    const byte a = level;
-    const byte b = level + (LEVEL_MAX / 3);
-    const byte c = level + (LEVEL_MAX / 3) * 2;
-    const byte sat = gHueLargeRing < 1 ? 0 : 255;
-    const byte NUM_LEDS = 6;
-    const byte levels[] = {LEVEL_MAX - c, LEVEL_MAX - b, LEVEL_MAX - a,
-                           c, b, a};  // NUM_LEDS
+void showRunningLights(byte speed, byte largeRingIndex, byte level) {
+  // Large Ring
+  const auto a = level;
+  const auto b = level + (LEVEL_MAX / 3);
+  const auto c = level + (LEVEL_MAX / 3) * 2;
+  const auto sat = gHueLargeRing < 1 ? 0 : 255;
+  const auto NUM_LEDS = 6;
+  const byte levels[] = { LEVEL_MAX - c, LEVEL_MAX - b, LEVEL_MAX - a,
+                          c, b, a };  // NUM_LEDS
 
-    for (byte seg = 0; seg < SEGMENT_SIZE; ++seg) {
-        const byte index = largeRingIndex + seg * NUM_LEDS_LARGE_SEGMENT;
-        for (byte offset = 0; offset < NUM_LEDS; ++offset) {
-            ledsLargeRing[(index + offset) % NUM_LEDS_LARGE_RING] =
-                CHSV(gHueLargeRing, sat, min(levels[offset], gBrightness));
-        }
+  for (auto seg = 0; seg < SEGMENT_SIZE; ++seg) {
+    const auto index = largeRingIndex + seg * NUM_LEDS_LARGE_SEGMENT;
+    for (auto offset = 0; offset < NUM_LEDS; ++offset) {
+      ledsLargeRing[(index + offset) % NUM_LEDS_LARGE_RING] =
+        CHSV(gHueLargeRing, sat, min(levels[offset], gBrightness));
     }
+  }
 
-    // Small ring
-    const word smallRingSpeed = max(1, speed / 5);
-    setSmallRing(smallRingSpeed);
+  // Small ring
+  const auto smallRingSpeed = max(1, speed / 5);
+  setSmallRing(smallRingSpeed);
 
-    for (byte seg = 0; seg < SEGMENT_SIZE; ++seg) {
-        ledsLargeRing[(largeRingIndex + seg * NUM_LEDS_LARGE_SEGMENT) % NUM_LEDS_LARGE_RING] =
-            CRGB::Black;
-    }
+  for (auto seg = 0; seg < SEGMENT_SIZE; ++seg) {
+    ledsLargeRing[(largeRingIndex + seg * NUM_LEDS_LARGE_SEGMENT) % NUM_LEDS_LARGE_RING] =
+      CRGB::Black;
+  }
 
-    FastLED.show();
-    delay(10);
+  FastLED.show();
+  delay(10);
 }
 
 void readPots() {
-    gPulseSpeed = 1024 - analogRead(PIN_POT_PULSE_SPEED);
-    gHueSmallRing = 252 - map(analogRead(PIN_POT_SMALL_COLOR), 0, 1024, 0, 255);
-    gHueLargeRing = 252 - map(analogRead(PIN_POT_LARGE_COLOR), 0, 1024, 0, 255);
-    gBrightness = 254 - map(analogRead(PIN_POT_BRIGHTNESS), 0, 1024, 0, 255);
+  gPulseSpeed = 1024 - analogRead(PIN_POT_PULSE_SPEED);
+  gHueSmallRing = 252 - map(analogRead(PIN_POT_SMALL_COLOR), 0, 1024, 0, 255);
+  gHueLargeRing = 252 - map(analogRead(PIN_POT_LARGE_COLOR), 0, 1024, 0, 255);
+  gBrightness = 254 - map(analogRead(PIN_POT_BRIGHTNESS), 0, 1024, 0, 255);
 }
 
 // **************************************************************************************************************
 void loop() {
-    word speed = 1;
-    bool finishLoop = false;
+  auto speed = 1;
+  bool finishLoop = false;
 
-    for (byte largeRingIndex = 0; largeRingIndex < NUM_LEDS_LARGE_RING && !finishLoop; ++largeRingIndex) {
-        for (byte level = 0; level < LEVEL_MAX / 3; level += speed) {
+  for (auto largeRingIndex = 0; largeRingIndex < NUM_LEDS_LARGE_RING && !finishLoop; ++largeRingIndex) {
+    for (auto level = 0; level < LEVEL_MAX / 3; level += speed) {
 #ifdef READ_POTS
-            readPots();
+      readPots();
 #endif
-            if (gPulseSpeed > PULSE_SPEED_MAX) {
-                showSolid();
-                finishLoop = true;
-                break;
-            } else {
-                speed = max(1, map(gPulseSpeed, 0, PULSE_SPEED_MAX, 0, 150));
-                showRunningLights(speed, largeRingIndex, level);
-            }
-        }
+      if (gPulseSpeed > PULSE_SPEED_MAX) {
+        showSolid();
+        finishLoop = true;
+        break;
+      } else {
+        speed = max(1, map(gPulseSpeed, 0, PULSE_SPEED_MAX, 0, 150));
+        showRunningLights(speed, largeRingIndex, level);
+      }
     }
+  }
 
-    delay(1);
+  delay(1);
 }
